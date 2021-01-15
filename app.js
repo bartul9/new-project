dfd75b66bfe154a81b4117301fef63d48ff4aacf
bartul_9 <bartul123@outlook.com>
2021-01-15 21:51:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
 const nav = document.querySelector(".nav");
-const section1 = document.querySelector(".spanLink");
+const section1 = document.querySelector(".section--one");
+
 // Nav links hover effects
 
-const handleHover = function (e) {
-  if (e.target.classList.contains("nav-link")) {
-    const link = e.target;
-    const siblings = link.closest(".nav").querySelectorAll(".nav-link");
-    const logo = link.closest(".nav").querySelector("img");
+// const handleHover = function (e) {
+//   if (e.target.classList.contains("nav-link")) {
+//     const link = e.target;
+//     const siblings = link.closest(".nav").querySelectorAll(".nav-link");
+//     const logo = link.closest(".nav").querySelector("img");
 
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = this;
-    });
-    logo.style.opacity = this;
-  }
-};
+//     siblings.forEach((el) => {
+//       if (el !== link) el.style.opacity = this;
+//     });
+//     logo.style.opacity = this;
+//   }
+// };
 
-nav.addEventListener("mouseover", handleHover.bind(0.5));
+// nav.addEventListener("mouseover", handleHover.bind(0.5));
 
-nav.addEventListener("mouseout", handleHover.bind(1));
-
-// Sticky navbar scrolling effect
-
-const stickyNav = function (entries) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) nav.classList.add("sticky");
-  else nav.classList.remove("sticky");
-};
-
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0,
-  rootMargin: "+20px",
-});
-headerObserver.observe(section1);
-
-//  Responsive navbar
-
-// Chellange kata
+// nav.addEventListener("mouseout", handleHover.bind(1));
