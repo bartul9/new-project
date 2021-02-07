c53403ad156b15e995de5b7ce6b088cab0ad852b
bartul_9 <bartul123@outlook.com>
2021-02-07 12:02:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const section1 = document.querySelector(".section--one");
 
 // map
 
-var map = L.map("mapid").setView([52.52, 13.405], 10);
+var map = L.map("mapid").setView([52.52, 13.405], 8);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -13,3 +13,26 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 L.marker([52.52, 13.405]).addTo(map).openPopup();
+
+/// section fade in
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
